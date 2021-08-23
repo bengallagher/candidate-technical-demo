886e326e20b226543fff6f7187202afee65f29bd
Ben Gallagher <ben.gallagher@immersivelabs.co.uk>
2021-08-23 08:51:28 +0100

    Removed response metadata

--- src/app.py
+++ src/app.py
@@ -6,10 +6,16 @@ session = boto3.Session()
 
 def lambda_handler(event, context):
 
+    # Connect to STS service.
     client = session.client("sts")
 
+    # Make STS api call.
     sts_response = client.get_caller_identity()
 
+    # Remove metadata from response.
+    sts_response.pop("ResponseMetadata", None)
+
+    # Return formatted response object.
     return {
         "statusCode": 200,
         "body": json.dumps(sts_response),
